1d46a3fdcd96411d78d15692b613d0beafd5f99c
9T97 <win59813@gmail.com>
2022-09-17 09:22:11 +0630

    feat[db]: Change to UTC

--- config/database.php
+++ config/database.php
@@ -1,5 +1,5 @@
 <?php
-
+#phpcs:ignore;
 use Illuminate\Support\Str;
 
 return [
@@ -61,6 +61,7 @@ return [
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
+            'timezone' => '+00:00',
         ],
 
         'pgsql' => [
